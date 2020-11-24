3a63bbd49d1fa4f1254955284097efef08f4d1eb
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2020-11-25 06:33:22 +0900

    Get api key and secret key from environment variables.

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/M-Yamashita01/MeetingPariticipantsExporter
 go 1.15
 
 require (
+	github.com/cristalhq/jwt/v3 v3.0.4
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/yaegashi/msgraph.go v0.1.4
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
 )
--- main.go
+++ main.go
@@ -2,22 +2,53 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/httputil"
+	"time"
+
+	"github.com/cristalhq/jwt/v3"
+	"github.com/kelseyhightower/envconfig"
 )
 
+type Env struct {
+	SecretKey string `required:"true"`
+	ApiKey    string `required:"true"`
+}
+
 func main() {
-	var accessToken string
+	var goenv Env
+	if err := envconfig.Process("", &goenv); err != nil {
+		log.Printf("ERROR Environment is not set: %s", err)
+		return
+	}
+	fmt.Println(goenv)
+
+	// Generate access token by JWT
+	key := []byte(goenv.SecretKey)
+	signer, _ := jwt.NewSignerHS(jwt.HS256, key)
+
+	now := time.Now()
+	after := now.Add(time.Minute)
+	claims := jwt.StandardClaims{
+		Issuer:    goenv.ApiKey,
+		ExpiresAt: jwt.NewNumericDate(after),
+	}
+
+	builder := jwt.NewBuilder(signer)
+	token, _ := builder.Build(claims)
+	fmt.Printf("Access token : %s\n", token)
+
+	var tokenStr string
 	var meetingUUID string
-	fmt.Println("Input access token")
-	fmt.Scan(&accessToken)
+	tokenStr = token.String()
 	fmt.Println("Input meetingId")
 	fmt.Scan(&meetingUUID)
 
 	meetingParticipantsUrl := "https://api.zoom.us/v2/past_meetings/"
 	url := meetingParticipantsUrl + meetingUUID + "/participants"
 	req, _ := http.NewRequest("GET", url, nil)
-	bearerAccessToken := "Bearer " + accessToken
+	bearerAccessToken := "Bearer " + tokenStr
 	req.Header.Set("Authorization", bearerAccessToken)
 
 	dump, _ := httputil.DumpRequestOut(req, true)
